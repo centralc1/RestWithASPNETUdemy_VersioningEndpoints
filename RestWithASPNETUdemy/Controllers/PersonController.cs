49f128b112f2a48ad34758f269808040728916b3
Ricardo da Cunha <joaovitorleitesantos98@gmail.com>
2023-11-16 20:53:33 -0300

    Add project files.

---
+++ RestWithASPNETUdemy/Controllers/PersonController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Services.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiVersion("1")] 
+    [ApiController]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class PersonController : ControllerBase
+    {
+
+        private readonly ILogger<PersonController> _logger;
+        private IPersonService _personService;  
+        public PersonController(ILogger<PersonController> logger, IPersonService personService)
+        {
+            _logger = logger;
+            _personService = personService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_personService.FindAll());   
+
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var person = _personService.FindByID(id);
+            if (person == null) return NotFound();
+            return Ok(person);
+
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personService.Create(person));
+
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personService.Create(person));
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _personService.Delete(id);
+            return NoContent(); 
+
+        }
+    }
+}
+
